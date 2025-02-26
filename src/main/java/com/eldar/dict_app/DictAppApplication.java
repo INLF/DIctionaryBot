0e7305376dab6a6aed3b8eaa61cfa189820b9c1d
Eldar <elbro.ya@mail.ru>
2025-02-26 23:19:02 +0300

    init

---
+++ src/main/java/com/eldar/dict_app/DictAppApplication.java
@@ -0,0 +1,13 @@
+package com.eldar.dict_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DictAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DictAppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/eldar/dict_app/controller/TelegramController.java
@@ -0,0 +1,4 @@
+package com.eldar.dict_app.controller;
+
+public class TelegramController {
+}
